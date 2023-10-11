5426554cac2979303da7421b67427819698e96bf
pedrobarros0102 <pedrobarros0101@outlook.com>
2023-10-11 15:22:15 -0300

    Concluindo animações

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const origemBiblia = document.getElementById("biblia-enable");
 const destinoBiblia = document.getElementById("biblia-disable");
 
 function esconderBiblia(){
-    origemBiblia.style.display = "none";
-    destinoBiblia.style.display  ="block";
+    origemBiblia.classList.remove("block");
+    origemBiblia.classList.add("hidden");
+
+    destinoBiblia.classList.remove("hidden");
+    destinoBiblia.classList.add("block");
 }
 function descobrirBiblia(){
-    origemBiblia.style.display = "block";
-    destinoBiblia.style.display  ="none";
+    origemBiblia.classList.remove("hidden");
+    origemBiblia.classList.add("block");
+
+    destinoBiblia.classList.remove("block");
+    destinoBiblia.classList.add("hidden");
 }
